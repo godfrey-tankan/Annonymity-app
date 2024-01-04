2877e60695f2837255662bb87d656f062feb7738
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-01-04 15:57:38 +0200

    Login Endpoint

--- confessions/static/js/script.js
+++ confessions/static/js/script.js
@@ -1,17 +1,25 @@
 function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
+
+function setRandomGradient() {
+  const body = document.querySelector('body');
+  const color1 = getRandomColor();
+  const color2 = getRandomColor();
+  body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
   
-  function setRandomGradient() {
-    const body = document.querySelector('body');
-    const color1 = getRandomColor();
-    const color2 = getRandomColor();
-    body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
-  }
-  
-  setInterval(setRandomGradient, 2000);
+  // Reset the animation after it finishes
+  setTimeout(() => {
+    body.style.animation = '';
+  }, 4000);
+}
+
+window.addEventListener('load', () => {
+  setRandomGradient();
+  setInterval(setRandomGradient, 50000);
+});
